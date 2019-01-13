ed011967188aa5486857760f16322afb6961839f
Jeffrey Baldwin <jbaldwin01@snet.net>
2019-01-13 15:21:43 -0500

    Use API URL for image not available and handle no author or multiple authors

--- src/components/Book.js
+++ src/components/Book.js
@@ -14,7 +14,7 @@ class Book extends Component {
         <div className="book">
           <div className="book-top">
             <div className="book-cover" 
-              style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail || "/not-available.jpg"})` }}>
+              style={{ width: 128, height: 193, backgroundImage: `url(${(book.imageLinks && book.imageLinks.thumbnail) || "http://books.google.com/books/content?id=NLK2AAAAIAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"})` }}>
             </div>
             <div className="book-shelf-changer">
               <select value={book.shelf || "none"} onChange={(event) => changeShelf(book, event.target.value)}>
@@ -27,7 +27,9 @@ class Book extends Component {
             </div>
           </div>
           <div className="book-title">{book.title}</div>
-          <div className="book-authors">{book.authors || "Author not listed"}</div>
+          {book.authors && book.authors.map((author, i) => {
+            return <div key={i} className="book-authors">{author}</div>
+          })}
         </div>
       </li>
     )
